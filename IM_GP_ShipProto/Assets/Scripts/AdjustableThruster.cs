b4450f69cc0c4e731608d626de059336c8d9fb62
Lorenz Krautgartner <Lorenz.Krautgartner@gmail.com>
2016-03-29 17:45:55 +0200

    added different ship-prefabs changed some stuff doobydoo boobaloo

---
+++ IM_GP_ShipProto/Assets/Scripts/AdjustableThruster.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdjustableThruster : MonoBehaviour
+{
+
+    public KeyCode upKey;
+    public KeyCode downKey;
+
+    private float maxPower;
+    private float currentPower;
+    private float particleLifetime = 0.6f;
+
+
+    private Ship ship;
+
+    private Rigidbody2D body;
+    private GameObject afterBurner;
+
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+        ship = GetComponentInParent<Ship>();
+        afterBurner = transform.GetChild(0).gameObject;
+        afterBurner.SetActive(false);
+
+        currentPower = 0.0f;
+        maxPower = ship.thrusterPower;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(upKey))
+        {
+            currentPower += (maxPower / 5);
+            if (currentPower > maxPower) currentPower = maxPower;
+        }
+        if (Input.GetKeyDown(downKey))
+        {
+            currentPower -= (maxPower / 5);
+            if (currentPower < 0) currentPower = 0;
+        }
+
+        if (currentPower > 0)
+        {
+            body.AddForce(transform.rotation * Vector3.up * currentPower);
+            afterBurner.GetComponent<ParticleSystem>().startLifetime = particleLifetime * (((100.0f * currentPower) / maxPower) / 100);
+            afterBurner.SetActive(true);
+
+        }
+        else
+        {
+            afterBurner.SetActive(false);
+        }
+    }
+}
--- IM_GP_ShipProto/Assets/Scripts/Thruster.cs
+++ IM_GP_ShipProto/Assets/Scripts/Thruster.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Thruster : MonoBehaviour {
+public class Thruster : MonoBehaviour
+{
 
     public KeyCode key;
     private Ship ship;
@@ -9,21 +10,27 @@ public class Thruster : MonoBehaviour {
     private Rigidbody2D body;
     private GameObject afterBurner;
 
-	// Use this for initialization
-	void Start () {
-        body = GetComponentInParent<Rigidbody2D>();
+    // Use this for initialization
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
         ship = GetComponentInParent<Ship>();
         afterBurner = transform.GetChild(0).gameObject;
         afterBurner.SetActive(false);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    if (Input.GetKey(key)) {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(key))
+        {
             body.AddForce(transform.rotation * Vector3.up * ship.thrusterPower);
             afterBurner.SetActive(true);
-        } else {
+        }
+        else
+        {
             afterBurner.SetActive(false);
         }
-	}
+    }
 }
