dafb55ecd067e36eebc7b4a60b414a3ed8f94b64
unknown <dnl.rammer@gmail.com>
2016-03-30 13:32:11 +0200

    juicy stuff

--- IM_GP_ShipProto/Assets/Scripts/Thruster.cs
+++ IM_GP_ShipProto/Assets/Scripts/Thruster.cs
@@ -8,11 +8,15 @@ public class Thruster : MonoBehaviour {
 
     private Rigidbody2D body;
     private GameObject afterBurner;
+    private AudioSource audio;
+    private CameraShake shake;
 
 	// Use this for initialization
 	void Start () {
         body = GetComponentInParent<Rigidbody2D>();
         ship = GetComponentInParent<Ship>();
+        audio = GetComponent<AudioSource>();
+        shake = CameraShake.Instance;
         afterBurner = transform.GetChild(0).gameObject;
         afterBurner.SetActive(false);
 	}
@@ -25,5 +29,14 @@ public class Thruster : MonoBehaviour {
         } else {
             afterBurner.SetActive(false);
         }
+
+        if(Input.GetKeyDown(key)) {
+            audio.Play();
+            shake.StartPermaShake(0.16f);
+        } else if (Input.GetKeyUp(key)) {
+            audio.Stop();
+            shake.StopPermaShake();
+        }
+
 	}
 }
